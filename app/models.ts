b50b44c7ab82c61f959b132c574cf4c106bb5da4
Yatin Chokshi <yatin.chokshi@cbre.com>
2025-10-19 15:32:32 +0530

    added lv power cable

---
+++ app/models.ts
@@ -0,0 +1,11 @@
+export type Product = {
+    Cable_Name: number;
+    Product_Type: string;
+    Standards?: string;
+    Certifications?: string;
+    Key_Features?:string;
+    Short_Description: string;
+    Full_Description: string;
+    Image_Path: string;
+    Brochure_Path: string;
+  };
